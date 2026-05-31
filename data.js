8330ebe97375485693910cf1d85bee4ed0637d04
MightyLabs <auslabib@gmail.com>
2026-06-01 07:21:37 +1000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+// Guild War Player Data - Maximum 30 players per guild
+// Where Winds Meet - Guild vs Guild
+let members = [
+    { id: 1, name: "Player1", role: "Tank", team: "", weapon1: "Thundercry Blade", weapon2: "Nameless Spear" },
+    { id: 2, name: "Player2", role: "Healer", team: "", weapon1: "Panacea Fan", weapon2: "Soulshade Umbrella" },
+    { id: 3, name: "Player3", role: "DPS", team: "", weapon1: "Nameless Sword", weapon2: "Nameless Spear" }
+];
